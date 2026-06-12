a03fa8a83e0a7bfaa88d0c5fdf73ddb6d25fd5a7
YeongGi Lee <young2too13@gmail.com>
2026-06-13 00:32:23 +0900

    Configure Supabase rankings

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.LUMIA_TACTICS_CONFIG = {
   supabase: {
-    url: "",
-    anonKey: "",
+    url: "https://vuvljodrufdzbzbyzaxf.supabase.co",
+    anonKey: "sb_publishable_QW3oRV0SSjOFC0ig5mp1Kw_7Iuge0b3",
     table: "rankings",
   },
 };
